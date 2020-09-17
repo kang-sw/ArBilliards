f190c0d2ec1a17263d7beb608badcc26d8a1ce0e
Stephen Kang <ki6080@gmail.com>
2020-09-17 12:45:32 +0900

    Add physics object classes

--- Projects/UnityAr/Assets/ArBilliards/Scripts/BilliardsSimulation.cs
+++ Projects/UnityAr/Assets/ArBilliards/Scripts/BilliardsSimulation.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using Boo.Lang.Runtime;
+using JetBrains.Annotations;
+using UnityEditor.UIElements;
 using UnityEngine;
 
 namespace Billiards.Simulation
@@ -13,15 +16,16 @@ namespace Billiards.Simulation
 		public Vector3 White;
 	}
 
-
-// TODO: 구 - 구 충돌, 구 - 정적 평면 ==> 직선 - 정적 평면 충돌
-// TODO: 반발 계수 공식 
-// TODO: 정해진 time step에 대해 모든 충돌체 iterate하여 가장 
-public class Context
+	// TODO: 구 - 구 충돌, 구 - 정적 평면 ==> 직선 - 정적 평면 충돌
+	// TODO: 반발 계수 공식 
+	// TODO: 정해진 time step에 대해 모든 충돌체 iterate하여 가장 
+	public class Context
 	{
 		#region Simulation Properties
 
 		public (float Min, float Max) ImpactRange;
+		public float BallDampingCoeff = 0.02f;
+		public float RestitutionCoeff = 0.87f;
 
 		#endregion
 
@@ -69,4 +73,142 @@ public class Context
 		}
 
 	}
+
+	enum PhysType
+	{
+		Sphere,
+		StaticPlane
+	}
+
+	internal abstract class PhysObject
+	{
+		public PhysType Type { get; protected set; }
+		public float Mass = 0;
+		public Vector3 Position = new Vector3();
+		public Vector3 Velocity = new Vector3();
+		public float DampingCoeff = 0.001f;
+		public float RestitutionCoeff = 1.0f;
+
+		public abstract float? CalcMinContactTime(PhysObject other);
+		public abstract Contact? AdvanceSimulation(PhysObject other, float delta);
+
+		public struct Contact
+		{
+			public (Vector3 Pos, Vector3 Vel) A, B;
+			public float Time;
+		}
+	}
+
+	internal class PhysSphere : PhysObject
+	{
+		public float Radius = 1.0f;
+
+		public PhysSphere()
+		{
+			Type = PhysType.Sphere;
+		}
+
+		public override float? CalcMinContactTime(PhysObject other)
+		{
+			switch (other.Type)
+			{
+			case PhysType.Sphere:
+				return CalcContact((PhysSphere)other);
+			case PhysType.StaticPlane:
+				return CalcContact((PhysStaticPlane)other);
+			default:
+				throw new ArgumentOutOfRangeException();
+			} 
+		}
+
+		public override Contact? AdvanceSimulation(PhysObject other, float delta)
+		{
+			throw new NotImplementedException();
+		}
+
+		public float? CalcContact(PhysSphere o)
+		{
+			const float SMALL_NUMBER = 1e-7f;
+
+			if (DampingCoeff < SMALL_NUMBER)
+			{
+				throw new AssertionFailedException("Damping coefficient must be larger than 0.");
+			}
+
+			if (Math.Abs(DampingCoeff - DampingCoeff) < SMALL_NUMBER)
+			{
+				throw new AssertionFailedException("Damping coefficient between spheres must be equal");
+			}
+
+			Vector3 P1 = Position, P2 = o.Position, V1 = Velocity, V2 = o.Velocity;
+			float r1 = Radius, r2 = o.Radius;
+			float alpha = DampingCoeff;
+			Vector3 A, B;
+
+			B = V2 - V1;
+			A = P2 - P1 + (1 / alpha) * B;
+
+			float a = Vector3.Dot(B, B);
+			float b = Vector3.Dot(A, B);
+			float c = Vector3.Dot(A, A) - (r2 + r1) * (r2 + r1);
+
+			float discr = b * b - a * c;
+
+			if (discr < SMALL_NUMBER)
+			{
+				return null;
+			}
+
+			discr = Mathf.Sqrt(discr);
+			float umin = (b - discr) / a;
+			float umax = (b + discr) / a;
+
+
+			float? returnAlphaNonZero(float u)
+			{
+				return u > 0.0f && u <= 1.0f ? Mathf.Log(u) : new float?();
+			}
+
+			if (umin <= 0)
+			{
+				return returnAlphaNonZero(umax);
+			}
+			else
+			{
+				return returnAlphaNonZero(umin);
+			} 
+		}
+
+		public float? CalcContact(PhysStaticPlane other)
+		{
+			return null;
+		}
+	}
+
+	internal class PhysStaticPlane : PhysObject
+	{
+		public PhysStaticPlane()
+		{
+			Type = PhysType.StaticPlane;
+		}
+
+		public override float? CalcMinContactTime(PhysObject other)
+		{
+			switch (other.Type)
+			{
+			case PhysType.Sphere:
+				return ((PhysSphere)other).CalcContact(this);
+			case PhysType.StaticPlane:
+				return null;
+			default:
+				throw new ArgumentOutOfRangeException();
+			} 
+		}
+
+		public override Contact? AdvanceSimulation(PhysObject other, float delta)
+		{
+			throw new NotImplementedException();
+		}
+	}
+
 }
